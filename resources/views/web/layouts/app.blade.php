4b721e779262a4a69b30643d0acea242ed6711da
angelo <angeloccuno@gmail.com>
2025-07-14 16:18:39 -0500

    Se configura prettier

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es" data-theme="emerald">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/web/app.css', 'resources/js/web/app.js'])
 	<title>@yield('title') - Intefruits Perú</title>
 </head>
 <body>
