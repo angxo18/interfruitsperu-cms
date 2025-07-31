55bcd4dbe357f45ee8feb583b9c55364e4f3c099
angelo <angeloccuno@gmail.com>
2025-07-31 13:27:05 -0500

    Se modifica navbar

--- resources/views/components/web/navbar.blade.php
+++ resources/views/components/web/navbar.blade.php
@@ -23,50 +23,35 @@
  ];
 @endphp
 
-<nav class="px-5 lg:px-28 bg-base-100" x-data="{ open: false }">
-	<div class="flex justify-between items-center h-20">
-		<div class="flex flex-shrink-0 items-center align-bottom space-x-2 py-3">
-			<x-lucide-globe class="w-12 h-12 text-red-500" />
-
-			<span class="font-bold text-xl lg:text-3xl inline-block">Interfruits Perú</span>
-		</div>
-
-		<!-- Botón Toggle (mobile) -->
-		<div class="flex items-center lg:hidden">
-			<button
-				x-on:click="open = !open"
-				type="button"
-				class="btn btn-ghost"
-				aria-controls="mobile-menu"
-				aria-expanded="false"
-			>
-				<x-lucide-menu class="w-6 h-6" x-show="!open" />
-				<x-lucide-x class="w-6 h-6" x-show="open" />
-			</button>
-		</div>
-
-		<!-- Menú (desktop) -->
-		<div class="px-2 hidden lg:flex gap-5">
-			@foreach ($items as $item)
-				<a
-					href="{{ $item['url'] }}"
-					class="font-semibold text-sm hover:text-primary duration-300 transition-colors text-gray-700"
-				>
-					{{ $item['name'] }}
-				</a>
-			@endforeach
-		</div>
+<nav x-data="{ open: false }" class="bg-white w-full flex justify-around h-20">
+	<div class="flex items-center gap-3">
+		<x-lucide-globe class="w-12 h-12 text-red-500" />
+		<span class="text-xl lg:text-2xl font-bold">Interfruits Perú</span>
 	</div>
-
-	<!-- Menú colapsable (mobile) -->
-	<div class="lg:hidden flex flex-col gap-1" x-show="open" x-transition>
-		@foreach ($items as $item)
-			<a
-				href="{{ $item['url'] }}"
-				class="block rounded-box hover:bg-base-200/30 px-3 py-2 uppercase font-semibold text-sm hover:text-primary duration-300 transition-colors"
-			>
-				{{ $item['name'] }}
-			</a>
-		@endforeach
+	<div
+		class="absolute overflow-hidden top-24 lg:static lg:flex lg:items-center bg-white w-[90%] lg:w-auto lg:h-auto lg:max-h-full transition-all duration-300 lg:px-1"
+		x-bind:class="open ? 'max-h-80' : 'max-h-0'"
+		x-cloak
+	>
+		<ul class="flex flex-col lg:flex-row gap-5 p-5 lg:p-0">
+			@foreach ($items as $item)
+				<li>
+					<a
+						href="{{ $item['url'] }}"
+						class="text-gray-800 hover:text-emerald-600 transition-colors duration-300 font-semibold"
+					>
+						{{ $item['name'] }}
+					</a>
+				</li>
+			@endforeach
+		</ul>
+	</div>
+	<div class="flex items-center lg:hidden">
+		<button
+			class="p-2 rounded cursor-pointer hover:bg-gray-200 transition-colors duration-200"
+			x-on:click="open = !open"
+		>
+			<x-lucide-menu class="w-6 h-6" />
+		</button>
 	</div>
 </nav>
