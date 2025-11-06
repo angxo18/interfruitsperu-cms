9c1e13a61ebda13e4d55a0a63c684f5084236c71
Angelo Ccuno <angeloccuno@gmail.com>
2025-11-06 11:30:08 -0500

    chore(admin): Se instala paquete spatie/laravel-query-builder

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -5,14 +5,35 @@ namespace App\Http\Controllers\Admin;
 use App\Helpers\Admin\PaginationHelper;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class UserController extends Controller
 {
-    public function index(): View
+    public function index(Request $request): View
     {
         $perPage = PaginationHelper::perPage();
-        $users = User::paginate($perPage)->withQueryString();
+
+        $users = QueryBuilder::for(User::class)
+            ->allowedFilters([
+                AllowedFilter::partial('name'),
+                AllowedFilter::partial('email'),
+                AllowedFilter::callback('search', function (Builder $query, $value) {
+
+                    $query->where(function ($q) use ($value) {
+                        $q->where('name', 'like', "%{$value}%")
+                            ->orWhere('email', 'like', "%{$value}%");
+                    });
+
+                }),
+            ])
+            ->paginate($perPage)
+            ->withQueryString();
+
+        // $users = User::paginate($perPage)->withQueryString();
 
         return view('admin.pages.users.index', ['users' => $users]);
     }
